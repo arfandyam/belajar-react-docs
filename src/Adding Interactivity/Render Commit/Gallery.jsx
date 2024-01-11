fcce8dcaf73f37ffd598186ad6329134709c6397
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-01-11 16:42:37 +0800

    Membuat componen gallery untuk menampilkan componen image

---
+++ src/Adding Interactivity/Render Commit/Gallery.jsx
@@ -0,0 +1,13 @@
+import Image from './Image';
+
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Inspiring Sculpture</h1>
+      <Image />
+      <Image />
+      <Image />
+    </section>
+  )
+}
+
