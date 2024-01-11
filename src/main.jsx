7208aadfdbd42c5541764ab7a73cd6f53262aa69
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-01-11 16:42:01 +0800

    menambah componen2 yg akan di render

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
+// import Button from './Adding Interactivity/Responding to Events/Adding Event Handlers.jsx'
+// import Toolbar from './Adding Interactivity/Responding to Events/Reading Props in Event Handlers.jsx'
+// import Toolbar from './Adding Interactivity/Responding to Events/Passing Event Handler as Props'
+import Toolbar from './Adding Interactivity/Responding to Events/Event Propagation'
+import { ToolbarStopPropagate } from './Adding Interactivity/Responding to Events/Event Propagation'
+import SignUp from './Adding Interactivity/Responding to Events/Preventing Default Behavior'
+// import Gallery from './Adding Interactivity/State A Components Memory/Adding a State Variable'
+// import Gallery from './Adding Interactivity/State A Components Memory/Giving a Component Multiple State Variables'
+import AppState from './Adding Interactivity/State A Components Memory/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Toolbar />
+    <ToolbarStopPropagate />
+    <SignUp />
+    <AppState />
+    {/* <Gallery /> */}
+    {/* <App /> */}
   </React.StrictMode>,
 )
