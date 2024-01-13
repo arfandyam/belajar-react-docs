858b81f5d9bf989d5417fd6b4265c60897264bf7
Arfandy Adimurfiq <arfandiadimurfiq@gmail.com>
2024-01-13 23:04:45 +0800

    membuat komponen bucket list immer untuk mengupdate elemen array state dengan syntax mutate dengan setting state menggunakan useImmer

---
+++ src/Adding Interactivity/Updating Arrays in a State/Write Concise Update Logic with Immer.jsx
@@ -0,0 +1,110 @@
+/**
+ * Objects are not really located “inside” arrays. They might appear to be “inside” in code, but 
+ * each object in an array is a separate value, to which the array “points”. This is why you need 
+ * to be careful when changing nested fields like list[0]. Another person’s artwork list may point 
+ * to the same element of the array!
+ * 
+ * When updating nested state, you need to create copies from the point where you want to update,
+ * and all the way up to the top level.
+ * 
+ * The problem on code like this:
+ * 
+ *    const myNextList = [...myList];
+ *    const artwork = myNextList.find(a => a.id === artworkId);
+ *    artwork.seen = nextSeen; // Problem: mutates an existing item
+ *    updateMyList(myNextList);
+ * 
+ * Although the myNextList array itself is new, the items themselves are the same as in the original
+ * myList array. So changing artwork.seen changes the original artwork item. That artwork item is 
+ * also in yourList, which causes the bug. Bugs like this can be difficult to think about, but 
+ * thankfully they disappear if you avoid mutating state.
+ * 
+ * You can use map to substitute an old item with its updated version without mutation.
+ * 
+ *    updateMyList(myList.map(artwork => {
+ *      if (artwork.id === artworkId) {
+ *        // Create a *new* object with changes
+ *        return { ...artwork, seen: nextSeen };
+ *      } else {
+ *        // No changes
+ *        return artwork;
+ *      }
+ *    }));
+ */
+
+import { useImmer } from 'use-immer';
+import PropTypes from 'prop-types'
+
+// let nextId = 3;
+const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+];
+
+export default function BucketListImmer(){
+  const [myList, updateMyList] = useImmer(initialList);
+  const [yourList, updateYourList] = useImmer(initialList);
+
+  function handleMyListToggle(artworkId, nextSeen){
+    updateMyList(draft => {
+      const artwork = draft.find(a => a.id === artworkId);
+      artwork.seen = nextSeen;
+    });
+  }
+
+  function handleYourListToggle(artworkId, nextSeen){
+    updateYourList(draft => {
+      const artwork = draft.find(a => a.id === artworkId)
+      artwork.seen = nextSeen
+    });
+  }
+
+  return (
+    <>
+      <h1>My List of Art to See:</h1>
+      <ItemList
+        artworks = {myList}
+        onToggle = {handleMyListToggle}
+      />
+      <h1>Your List of Art to See:</h1>
+      <ItemList
+        artworks = {yourList}
+        onToggle = {handleYourListToggle}
+      />
+    </>
+    
+  )
+}
+
+function ItemList({artworks, onToggle}){
+  return (
+    <ul>
+      {
+        artworks.map(artwork => (
+
+          <li key={artwork.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={artwork.seen}
+                onChange={e => {
+                  onToggle(
+                    artwork.id,
+                    e.target.checked,
+                  )
+                }}
+              />
+              {artwork.title}
+            </label>
+          </li>
+        ))
+      }
+    </ul>
+  )
+}
+
+ItemList.propTypes = {
+  artworks: PropTypes.array,
+  onToggle: PropTypes.bool
+}
